49159233a7f4e1d763cb6cd2dbfe37359441ec20
amol951 <laisram.amol@gmail.com>
2026-09-23 05:12:53 +0000

    Initial student portal website

---
+++ script.js
@@ -0,0 +1,7 @@
+```javascript
+function showMessage() {
+    document.getElementById("message").innerText =
+        "Welcome to the Student Portal!";
+}
+```
+1~function showMessage() { document.getElementById("message").innerText = "Welcome to the Student Portal!"; }
